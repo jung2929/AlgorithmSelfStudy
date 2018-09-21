4e0f4a48cd2d71865189d9f171dbaf51505253d6
JerryJung-MacBook Pro <jwh1269@naver.com>
2018-09-21 13:11:38 +0900

    3개의 값 중 중간값 찾기

--- src/com/jerryjung/Main.java
+++ src/com/jerryjung/Main.java
@@ -1,8 +1,28 @@
 package com.jerryjung;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("정제리 알고리즘 셀프 스터디");
+        final Scanner scanner = new Scanner(System.in);
+        final int parameter1 = scanner.nextInt();
+        final int parameter2 = scanner.nextInt();
+        final int parameter3 = scanner.nextInt();
+
+        int median = 0;
+
+        if (parameter1 > parameter2) {
+            if (parameter2 > parameter3) median = parameter2;
+            else if (parameter1 > parameter3) median = parameter3;
+            else median = parameter1;
+        } else {
+            // parameter2 > parameter1
+            if (parameter1 > parameter3) median = parameter1;
+            else if (parameter2 > parameter3) median = parameter3;
+            else median = parameter2;
+        }
+
+        System.out.print(median);
     }
 }
