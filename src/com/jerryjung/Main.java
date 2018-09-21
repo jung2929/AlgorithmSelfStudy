d9d9d57988b79f32bc72ef76b0e421088b9578da
JerryJung-MacBook Pro <jwh1269@naver.com>
2018-09-21 13:32:56 +0900

    문제, 입력, 출력, 예제 입력, 예제 출력 주석 추가

--- src/com/jerryjung/Main.java
+++ src/com/jerryjung/Main.java
@@ -2,6 +2,18 @@ package com.jerryjung;
 
 public class Main {
 
+    /**
+     * 문제 :
+     *
+     * 입력 :
+     *
+     * 출력 :
+     *
+     * 예제 입력 :
+     *
+     * 예제 출력 :
+     *
+     */
     public static void main(String[] args) {
         System.out.println("정제리 알고리즘 셀프 스터디");
     }
